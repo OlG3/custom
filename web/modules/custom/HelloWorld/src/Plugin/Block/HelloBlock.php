388a53e7f027631ecb91e064cb413f1441786297
geronimo <o.supruniuk@internetdevels.com>
2018-10-11 12:34:25 +0300

    Default field value

--- web/modules/custom/HelloWorld/src/Plugin/Block/HelloBlock.php
+++ web/modules/custom/HelloWorld/src/Plugin/Block/HelloBlock.php
@@ -82,5 +82,15 @@ class HelloBlock extends BlockBase implements BlockPluginInterface{
   }*/
 
 
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    $default_config = \Drupal::config('hello_world.settings');
+    return [
+      'hello_block_name' => $default_config->get('hello.name'),
+    ];
+  }
+
 }
 
